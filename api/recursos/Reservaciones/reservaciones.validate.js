a54512292fb048173dc01e958e5678b46e719e12
Fernando Tumax <fernandotumax52@gmail.com>
2021-05-21 15:02:48 -0600

    Room validacion en reservacion

--- api/recursos/Reservaciones/reservaciones.validate.js
+++ api/recursos/Reservaciones/reservaciones.validate.js
@@ -6,7 +6,7 @@ const blueprintReservation = Joi.object({
   fechaSalida: Joi.required(),
   numeroTarjeta: Joi.number().positive().min(16).required(),
   totalPagar: Joi.number().positive().precision(2).required(),
-  room: Joi.allow('').optional(),
+  room: Joi.array().allow(''),
 });
 
 let validarReservacion = (req, res, next) => {
